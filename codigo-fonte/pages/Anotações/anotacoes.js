73afbeee451da38f2f38844c2d2816712b79a894
damian-nestea <damian@hotmail.cz>
2024-06-22 10:10:29 -0300

    feat adição de funcionalidade busca de anotações por título ou conteúdo

--- codigo-fonte/pages/Anotações/anotacoes.js
+++ codigo-fonte/pages/Anotações/anotacoes.js
@@ -1,41 +1,42 @@
 // armazenamento local
 function salvarAnotacoes(anotacoes) {
-  localStorage.setItem('anotacoes', JSON.stringify(anotacoes));
+  localStorage.setItem("anotacoes", JSON.stringify(anotacoes));
 }
 
 // gerenciar o modal
 function abrirModalNovaAnotacao() {
-  document.getElementById('modalCriarEditar').style.display = 'block';
+  document.getElementById("modalCriarEditar").style.display = "block";
   limparFormularioModal();
 }
 
 function abrirModalEditarAnotacao(indiceAnotacao) {
-  document.getElementById('modalCriarEditar').style.display = 'block';
+  document.getElementById("modalCriarEditar").style.display = "block";
   preencherModal(indiceAnotacao);
 }
 
 function limparFormularioModal() {
-  const tituloInput = document.getElementById('titulo');
-  const conteudoTextarea = document.getElementById('conteudo');
-  document.getElementById('modalCriarEditar').dataset.indiceAnotacao = '';
-  tituloInput.value = '';
-  conteudoTextarea.value = '';
+  const tituloInput = document.getElementById("titulo");
+  const conteudoTextarea = document.getElementById("conteudo");
+  document.getElementById("modalCriarEditar").dataset.indiceAnotacao = "";
+  tituloInput.value = "";
+  conteudoTextarea.value = "";
 }
 
 function fecharModal() {
-  document.getElementById('modalCriarEditar').style.display = 'none';
+  document.getElementById("modalCriarEditar").style.display = "none";
 }
 
 function preencherModal(indiceAnotacao) {
-  const tituloInput = document.getElementById('titulo');
-  const conteudoTextarea = document.getElementById('conteudo');
+  const tituloInput = document.getElementById("titulo");
+  const conteudoTextarea = document.getElementById("conteudo");
 
   const anotacao = recuperarAnotacoes()[indiceAnotacao];
 
-  tituloInput.value = anotacao.titulo || '';
-  conteudoTextarea.value = anotacao.conteudo || '';
+  tituloInput.value = anotacao.titulo || "";
+  conteudoTextarea.value = anotacao.conteudo || "";
 
-  document.getElementById('modalCriarEditar').dataset.indiceAnotacao = indiceAnotacao;
+  document.getElementById("modalCriarEditar").dataset.indiceAnotacao =
+    indiceAnotacao;
 }
 
 // gerenciar anotações
@@ -47,14 +48,14 @@ function carregarAnotacoes() {
   const anotacoes = recuperarAnotacoes();
   if (anotacoes.length > 0) {
     preencherListaAnotacoes(anotacoes);
-    console.log('Anotações carregadas com sucesso!');
+    console.log("Anotações carregadas com sucesso!");
   } else {
     preencherListaAnotacoes(anotacoes);
   }
 }
 
 function recuperarAnotacoes() {
-  const anotacoesSalvas = localStorage.getItem('anotacoes');
+  const anotacoesSalvas = localStorage.getItem("anotacoes");
   if (anotacoesSalvas) {
     return JSON.parse(anotacoesSalvas);
   } else {
@@ -63,27 +64,27 @@ function recuperarAnotacoes() {
 }
 
 function criarElementoAnotacao(anotacao, index) {
-  const li = document.createElement('li');
-  li.classList.add('anotacao');
+  const li = document.createElement("li");
+  li.classList.add("anotacao");
 
   li.dataset.index = index;
 
-  const h3 = document.createElement('h3');
+  const h3 = document.createElement("h3");
   h3.textContent = anotacao.titulo;
   li.appendChild(h3);
 
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.textContent = anotacao.conteudo;
   li.appendChild(p);
 
-  const btnExcluir = document.createElement('button');
-  btnExcluir.classList.add('btn-excluir');
+  const btnExcluir = document.createElement("button");
+  btnExcluir.classList.add("btn-excluir");
   btnExcluir.innerHTML = '<i class="fa fa-trash"></i>';
   btnExcluir.onclick = () => excluirAnotacao(anotacao);
   li.appendChild(btnExcluir);
 
-  const btnEditar = document.createElement('button');
-  btnEditar.classList.add('btn-editar');
+  const btnEditar = document.createElement("button");
+  btnEditar.classList.add("btn-editar");
   btnEditar.innerHTML = '<i class="fa fa-pen  "></i>';
   btnEditar.onclick = () => abrirModalEditarAnotacao(index);
   li.appendChild(btnEditar);
@@ -93,22 +94,22 @@ function criarElementoAnotacao(anotacao, index) {
 
 // excluir anotação
 function excluirAnotacao(indice) {
-  if (confirm('Tem certeza que deseja excluir esta anotação?')) {
+  if (confirm("Tem certeza que deseja excluir esta anotação?")) {
     let anotacoes = recuperarAnotacoes();
     anotacoes.splice(indice, 1);
     salvarAnotacoes(anotacoes);
     carregarAnotacoes();
-    console.log('Anotação excluída com sucesso!');
+    console.log("Anotação excluída com sucesso!");
   }
 }
 
 function mostrarMensagemErro(mensagem) {
-  alert('Erro: ' + mensagem);
+  alert("Erro: " + mensagem);
 }
 
 function preencherListaAnotacoes(anotacoes) {
-  const listaAnotacoes = document.querySelector('.lista-anotacoes');
-  listaAnotacoes.innerHTML = '';
+  const listaAnotacoes = document.querySelector(".lista-anotacoes");
+  listaAnotacoes.innerHTML = "";
 
   anotacoes.forEach((anotacao, index) => {
     const anotacaoHTML = criarElementoAnotacao(anotacao, index);
@@ -118,7 +119,7 @@ function preencherListaAnotacoes(anotacoes) {
 
 // atualizar a anotação na lista após salvar ou excluir
 function atualizarAnotacaoNaLista(indice, novaAnotacao) {
-  const listaAnotacoes = document.querySelector('.lista-anotacoes');
+  const listaAnotacoes = document.querySelector(".lista-anotacoes");
   const anotacaoHTML = criarElementoAnotacao(novaAnotacao);
 
   if (indice > -1 && indice < listaAnotacoes.children.length) {
@@ -130,16 +131,18 @@ function atualizarAnotacaoNaLista(indice, novaAnotacao) {
 
 // salvar anotação
 function salvarAnotacao() {
-  const tituloInput = document.getElementById('titulo');
-  const conteudoTextarea = document.getElementById('conteudo');
+  const tituloInput = document.getElementById("titulo");
+  const conteudoTextarea = document.getElementById("conteudo");
   const indiceAnotacao =
-    parseInt(document.getElementById('modalCriarEditar').dataset.indiceAnotacao) || -1;
+    parseInt(
+      document.getElementById("modalCriarEditar").dataset.indiceAnotacao,
+    ) || -1;
 
   const titulo = tituloInput.value.trim();
   const conteudo = conteudoTextarea.value.trim();
 
   if (!titulo || !conteudo) {
-    mostrarMensagemErro('Título e conteúdo são obrigatórios!');
+    mostrarMensagemErro("Título e conteúdo são obrigatórios!");
     return;
   }
 
@@ -161,24 +164,51 @@ function salvarAnotacao() {
 
   salvarAnotacoes(anotacoes);
   fecharModal();
-  console.log('Anotação salva com sucesso!');
+  console.log("Anotação salva com sucesso!");
 }
 
 // Inicialização da página
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   carregarAnotacoes(); // Carrega as anotações ao carregar a página
-  const btnCadastreSe = document.querySelector('.btn-cadastre-se');
-  const btnNovaAnotacao = document.querySelector('.btn-nova-anotacao');
+  const btnCadastreSe = document.querySelector(".btn-cadastre-se");
+  const btnNovaAnotacao = document.querySelector(".btn-nova-anotacao");
   // Adiciona manipuladores de eventos para os botões
-  btnCadastreSe.addEventListener('click', abrirModalCadastro);
-  btnNovaAnotacao.addEventListener('click', abrirModalNovaAnotacao);
+  btnCadastreSe.addEventListener("click", abrirModalCadastro);
+  btnNovaAnotacao.addEventListener("click", abrirModalNovaAnotacao);
 });
 
-const botaoLogout = document.querySelector('#btn-logout');
+const botaoLogout = document.querySelector("#btn-logout");
 
-botaoLogout.addEventListener('click', function logoutUsuario() {
-  if (confirm('Tem certeza que deseja sair?')) {
-    localStorage.setItem('isLogado', false);
-    window.location.href = '../home/index.html';
+botaoLogout.addEventListener("click", function logoutUsuario() {
+  if (confirm("Tem certeza que deseja sair?")) {
+    localStorage.setItem("isLogado", false);
+    window.location.href = "../home/index.html";
   }
 });
+
+// Buscar anotações
+
+const buscaAnotacaoInput = document.querySelector("#campoBusca input");
+function buscaAnotacoes() {
+  const anotacoes = recuperarAnotacoes();
+  const anotacaoABuscar = buscaAnotacaoInput.value;
+
+  if (anotacaoABuscar != "") {
+    const anotacoesFiltradas = anotacoes.filter(
+      (anotacao) =>
+        anotacao.titulo.toLowerCase().includes(anotacaoABuscar) ||
+        anotacao.conteudo.toLowerCase().includes(anotacaoABuscar),
+    );
+    buscaAnotacaoInput.value = "";
+    if (!!anotacoesFiltradas.length) {
+      preencherListaAnotacoes(anotacoesFiltradas);
+    } else {
+      window.alert("Não existem anotações com o termo buscado!!!");
+    }
+  } else {
+    window.alert("Digite um termo para procurar sua anotação...");
+  }
+}
+
+const buscaAnotacaoBtn = document.querySelector("#buscaAnotacaoBtn");
+buscaAnotacaoBtn.addEventListener("click", buscaAnotacoes);
